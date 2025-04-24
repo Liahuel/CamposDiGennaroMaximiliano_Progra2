a30bc719d1fb0c5d70188c513ac8e4cdfa5a976e
tropus <lihue.digennaro@gmail.com>
2025-04-24 17:52:33 -0300

    TP1

--- src/estructuras/structure/implementation/Definiciones/Paciente.java
+++ src/estructuras/structure/implementation/Definiciones/Paciente.java
@@ -6,28 +6,25 @@ public class Paciente {
     private final int nivelDePrioridad;
     private boolean pacienteAtendido;
 
-    // Constructor
+
     public Paciente(String nombre, int nivelDePrioridad) {
         this.nombre = nombre;
         this.nivelDePrioridad = nivelDePrioridad;
         this.pacienteAtendido = false;
     }
 
-    // Método para ver el nivel de prioridad
+
     public int verNivelDePrioridad() {
         return nivelDePrioridad;
     }
 
-    // Método para verificar si el paciente fue atendido
     public boolean estaAtendido() {
         return pacienteAtendido;
     }
-    // Método para cambiar si el paciente a pacienteatendido
     public void AtenderPaciente() {
         this.pacienteAtendido = true;
     }
 
-    // Método para mostrar el nombre del paciente
     public String mostrarNombrePaciente() {
         return nombre;
     }
