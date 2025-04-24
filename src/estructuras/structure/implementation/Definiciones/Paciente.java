5955f687877b528c7a66eef4b0e7703d33367f1f
tropus <lihue.digennaro@gmail.com>
2025-04-24 17:53:19 -0300

    TP1

--- src/estructuras/structure/implementation/Definiciones/Paciente.java
+++ src/estructuras/structure/implementation/Definiciones/Paciente.java
@@ -4,13 +4,11 @@ public class Paciente {
     // Atributos
     private final String nombre;
     private final int nivelDePrioridad;
-    private boolean pacienteAtendido;
 
 
     public Paciente(String nombre, int nivelDePrioridad) {
         this.nombre = nombre;
         this.nivelDePrioridad = nivelDePrioridad;
-        this.pacienteAtendido = false;
     }
 
 
@@ -18,12 +16,6 @@ public class Paciente {
         return nivelDePrioridad;
     }
 
-    public boolean estaAtendido() {
-        return pacienteAtendido;
-    }
-    public void AtenderPaciente() {
-        this.pacienteAtendido = true;
-    }
 
     public String mostrarNombrePaciente() {
         return nombre;
